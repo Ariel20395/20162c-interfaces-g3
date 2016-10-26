cd531c2f7dbd1b3e5376a7af5625b122bd8119a5
ariel20395 <arielramirez201395@gmail.com>
2016-10-26 18:55:40 -0300

    se elimino factory.js

--- rankit_servicios/xtrest/src/main/webapp/js/factory.js
+++
@@ -1,7 +0,0 @@
-rankitApp.factory('Usuarios', function($resource) {
-    return $resource('/usuarios');
-});
-
-rankitApp.factory('Ranking', function($resource) {
-    return $resource('/ranking/:nombre:tipo:calificaciones:ranking');
-});
